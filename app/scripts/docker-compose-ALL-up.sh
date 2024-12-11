990d72a8602f1fb8eb76a1856f3e7f2ae1808eab
mitchell <mitchell.suzuki@1konto.com>
2024-12-11 16:37:58 -0700

    2: added docker-compose scripts and config for local dev

---
+++ app/scripts/docker-compose-ALL-up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get the absolute path of the script
+SCRIPT_PATH=$(realpath "${0}")
+
+# Get the parent directory of the script
+PARENT_DIR=$(dirname "${SCRIPT_PATH}")
+
+bash -c ${PARENT_DIR}/redis/docker-compose-up.sh
